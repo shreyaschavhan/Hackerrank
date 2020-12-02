3cb43fb18813bec567d3b14088f9c87e8be11024
Shreyas Chavhan <68887544+shreyaschavhan@users.noreply.github.com>
2020-12-02 22:27:59 +0530

    Add files via upload

---
+++ Algorithms/Warmup/plus-minus.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the plusMinus function below.
+void plusMinus(vector<int> arr, int n){
+    double positive = 0;
+    double negative = 0;
+    double zeros = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i] > 0){
+            positive++;
+        }
+        else if(arr[i] < 0){
+            negative++;
+        }
+        else{
+            zeros++;
+        }
+    }
+    n = double(n);
+    // cout << positive << endl << negative << endl << zeros << endl;
+    cout << fixed << setprecision(6) << positive/n << endl;
+    cout << fixed << setprecision(6) << negative/n << endl;
+    cout << fixed << setprecision(6) << zeros/n << endl;
+
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    plusMinus(arr, n);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
